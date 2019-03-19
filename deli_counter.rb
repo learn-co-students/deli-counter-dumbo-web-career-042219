bd42afd1ee7e91a5f48951db24993ec93f5986cb
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-19 14:56:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
-  the_line = "The line is currently: "
+  the_line = "The line is currently:"
   if katz_deli.size > 0
-    katz_deli.each.with_index(1) {|x, index| the_line +=("#{index}. #{x} ")}
+    katz_deli.each.with_index(1) {|x, index| the_line +=(" #{index}. #{x}")}
     puts the_line
   end
   if katz_deli.size == 0
