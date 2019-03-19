f1438e200d32e565cfa83e8b2dd3f68384047313
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-19 15:42:30 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,10 @@ def line(katz_deli)
   end
 end 
     
+    # katz_deli.each.with_index(1) {|x, index| 
+    
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+  
