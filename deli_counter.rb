f5f439847c2720d3351329cd9914f7264be2efe9
Kenton Archer <kenton8654@yahoo.com>
2019-04-12 21:01:48 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+
+
+def line (katz_deli)
+ array = []
+
+katz_deli.each_with_index do |idk, obj|
+
+    array.push(" The line is curretnly serving: #{idx +=1 - katz_deli.length }. #{obj += 1 - katz_deli.length}")
+    end
+
+    if line(katz_deli) == 0
+     puts "The line is currently empty"
+
+    end
+end
